0edbf4dadddf3e5361a7ead0e2bc45b520b1aaa3
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2021-09-12 21:07:21 +0200

    added  the burger ingredient component implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layouts/Layout';
+import BurgerBuilder from './containers/burger-builder/BurgerBuilder';
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
 
--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
+
 import Aux from '../../hoc/Auxillary'
+import classes from './layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, sideDrawer, backdrop</div>
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
     </Aux>
