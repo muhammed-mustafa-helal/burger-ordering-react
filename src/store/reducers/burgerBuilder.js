1a0e07d7a88b110b525f506656a74ea9bf823965
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2021-09-24 00:32:04 +0200

    refactored actions and containers and enabled redux devtools

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     ingredients: {
@@ -12,7 +12,7 @@ const initialState = {
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
-    cheese: 0.4,
+    cheese: 0.4, 
     meat: 1.3,
     bacon: 0.7
 };
