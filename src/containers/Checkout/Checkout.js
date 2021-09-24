b1aaf2f072032fa54a730074f9c26bd3c344e52e
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2021-09-24 02:13:13 +0200

    order and burgerBuilder reducers combined to manage loading and other state variables

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -15,7 +15,8 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
+        let summary = <Redirect to="/" />
+        if (this.props.ings) {
             <div>
                 <CheckoutSummary
                     ingredients={this.props.ings}
@@ -25,13 +26,16 @@ class Checkout extends Component {
                     path={this.props.match.path + '/contact-data'}
                     component={ContactData} />
             </div>
+        }
+        return (
+            { summary }
         );
     }
 }
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients
     }
 }
 
